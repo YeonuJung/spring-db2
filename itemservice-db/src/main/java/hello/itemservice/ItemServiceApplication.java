6b4d6398728bdb937e21952dea23977c120ff169
Jung Yeonwoo <gsh95214@gmail.com>
2025-05-16 13:19:41 +0900

    데이터 접근 기술 - 테스트

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -26,4 +26,14 @@ public class ItemServiceApplication {
         return new TestDataInit(itemRepository);
     }
 
+    // @Bean
+    // @Profile("test")
+    // public DataSource dataSource() {
+    //     DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    //     dataSource.setDriverClassName("org.h2.Driver");
+    //     dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+    //     dataSource.setUsername("sa");
+    //     dataSource.setPassword("");
+    //     return dataSource;
+    // }
 }
